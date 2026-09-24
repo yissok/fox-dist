963f8bbabd43339e2119abfe84163f17073e0fcb
static-app-publisher <static-app-publisher@users.noreply.github.com>
2026-09-24 12:31:54 +0000

    Publish 4bd67c3e06e0d896f5d81d764a2e0006cbce68e8

---
+++ releases/4bd67c3e06e0d896f5d81d764a2e0006cbce68e8-35999590640-1/_app/immutable/nodes/0.hBfOgrBX.js
@@ -0,0 +1,1 @@
+import{c as s,a as c}from"../chunks/BnXDMzER.js";import{b as p,E as i,N as m}from"../chunks/BROl-AM_.js";import{B as f}from"../chunks/WNtMOfi1.js";function l(a,r,...n){var o=new f(a);p(()=>{const e=r()??null;o.ensure(e,e&&(t=>e(t,...n)))},i)}function E(a,r){var n=s(),o=m(n);l(o,()=>r.children),c(a,n)}export{E as component};
